4ade9dc3409ac42d762dc2223a73d2a1cb5626c0
Susanna Ardigo <ardigs@usi.ch>
2018-05-03 15:07:15 +0200

    Initial Commit

---
+++ Virtual File System/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Virtual File System
+//
+//  Created by Susanna Ardigo' on 03.05.18.
+//  Copyright © 2018 Susanna Ardigo'. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
